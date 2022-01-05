6a5466425b130fba9e24db5fc2b97a56eb2ef6bd
Michal Charemza <michal@charemza.name>
2022-01-05 18:00:58 +0000

    refactor: single place to declare component

--- streamlit_gov_uk_components/__init__.py
+++ streamlit_gov_uk_components/__init__.py
@@ -18,25 +18,13 @@ _RELEASE = __name__ != '__main__'
 # your component frontend. Everything else we do in this file is simply a
 # best practice.
 
-if not _RELEASE:
-    _gov_uk_checkbox_func = components.declare_component(
-        # We give the component a simple, descriptive name ("my_component"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
-        "gov_uk_components_checkbox",
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
-        url="http://localhost:3001",
-    )
-else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to to the component's
-    # build directory:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend_checkbox/build")
-    _gov_uk_checkbox_func = components.declare_component("gov_uk_components_checkbox", path=build_dir)
-
+_gov_uk_checkbox_func = components.declare_component("gov_uk_components_checkbox",
+    **({
+        "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
+    } if _RELEASE else {
+        "url": "http://localhost:3001",
+    })
+)
 
 # Create a wrapper function for the component. This is an optional
 # best practice - we could simply expose the component function returned by
