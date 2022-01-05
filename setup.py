92201675f3b12a1ca21f870fc38d76c7dc953247
Michal Charemza <michal@charemza.name>
2022-01-05 14:53:01 +0000

    build(release): v0.0.0

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='streamlit-gov-uk-components',
+    version='0.0.0',
+    author='Department for International Trade',
+    author_email='sre@digital.trade.gov.uk',
+    description='A collection of Streamlit components that use or are inspired by the GOV.UK Design System',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uktrade/streamlit-gov-uk-components',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Archiving :: Compression',
+    ],
+    python_requires='>=3.7.4',
+    py_modules=[
+        'streamlit_gov_uk_components',
+    ],
+)
