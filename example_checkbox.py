761bdfece27836fa276405e755cbdaf273ffc0b8
Michal Charemza <michal@charemza.name>
2022-01-05 19:25:05 +0000

    feat: more accurate heading

--- example_checkbox.py
+++ example_checkbox.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_gov_uk_components import gov_uk_checkbox
 
-st.subheader("Component with constant args")
+st.subheader("Checkbox")
 
 selected = gov_uk_checkbox("Waste from animal carcasses", "waste")
 print('selected', selected)
