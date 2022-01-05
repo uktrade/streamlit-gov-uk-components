863abc790ac43e39356aec35f4366980b73bc992
Michal Charemza <michal@charemza.name>
2022-01-05 18:21:34 +0000

    fix: variable name

--- streamlit_gov_uk_components/__init__.py
+++ streamlit_gov_uk_components/__init__.py
@@ -1,10 +1,10 @@
 import os
 import streamlit.components.v1 as components
 
-_DEV = os.environ.get('GOV_UK_COMPONENTS_DEV')
+_DEV = os.environ.get('STREAMLIT_GOV_UK_COMPONENTS_DEV')
 _component_options = {
     "url": "http://localhost:3001",
-} if GOV_UK_COMPONENTS_DEV else {
+} if _DEV else {
     "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
 }
 
