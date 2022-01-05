69f5c80d7c6230ae9350fac73949365dcf904823
Michal Charemza <michal@charemza.name>
2022-01-05 18:17:44 +0000

    refactor: create dict with options

--- streamlit_gov_uk_components/__init__.py
+++ streamlit_gov_uk_components/__init__.py
@@ -2,14 +2,13 @@ import os
 import streamlit.components.v1 as components
 
 _DEV = not os.environ.get('DEV')
+_component_options = {
+    "url": "http://localhost:3001",
+} if _DEV else {
+    "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
+}
 
-_gov_uk_checkbox= components.declare_component("gov_uk_components_checkbox",
-    **({
-        "url": "http://localhost:3001",
-    } if _DEV else {
-        "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
-    })
-)
+_gov_uk_checkbox= components.declare_component("gov_uk_components_checkbox", **_component_options)
 
 def gov_uk_checkbox(name, key=None):
     return _gov_uk_checkbox(name=name, key=key, default=0)
