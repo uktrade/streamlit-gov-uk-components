d744590add7789e695c73a4ab77a37c485902687
Michal Charemza <michal@charemza.name>
2022-01-05 19:25:26 +0000

    feat: remove styling

--- example_checkbox.py
+++ example_checkbox.py
@@ -4,5 +4,4 @@ from streamlit_gov_uk_components import gov_uk_checkbox
 st.subheader("Checkbox")
 
 selected = gov_uk_checkbox("Waste from animal carcasses", "waste")
-print('selected', selected)
 st.markdown("Selected: %s" % selected)
