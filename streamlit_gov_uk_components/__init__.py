31e259a7a4eb5f11aecae9e4e0ffe1f647ccb88d
Michal Charemza <michal@charemza.name>
2022-01-05 18:12:34 +0000

    build: use _DEV variable from environment variables

--- streamlit_gov_uk_components/__init__.py
+++ streamlit_gov_uk_components/__init__.py
@@ -1,20 +1,20 @@
 import os
 import streamlit.components.v1 as components
 
-_RELEASE = __name__ != '__main__'
+_DEV = not os.environ.get('DEV')
 
 _gov_uk_checkbox= components.declare_component("gov_uk_components_checkbox",
     **({
-        "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
-    } if _RELEASE else {
         "url": "http://localhost:3001",
+    } if _DEV else {
+        "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend_checkbox/build")
     })
 )
 
 def gov_uk_checkbox(name, key=None):
     return _gov_uk_checkbox(name=name, key=key, default=0)
 
-if not _RELEASE:
+if _DEV:
     import streamlit as st
 
     st.subheader("Component with constant args")
