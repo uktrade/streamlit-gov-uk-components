80395ae469e611005165cf1469eb43f084157aa5
Michal Charemza <michal@charemza.name>
2022-01-05 18:37:07 +0000

    refactor: rename to clearer/more correct name

--- streamlit_gov_uk_components/frontend_checkbox/src/index.tsx
+++ streamlit_gov_uk_components/frontend_checkbox/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from "./MyComponent"
+import GovUkCheckbox from "./GovUkCheckbox"
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponent />
+    <GovUkCheckbox />
   </React.StrictMode>,
   document.getElementById("root")
 )
